97d22a5a4a127df0dd7a66fc925d13c610b54fe0
NFrevert <nils.frevert@gmail.com>
2025-04-14 20:15:43 +0200

    design reworked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,21 @@ import {MatIconModule} from '@angular/material/icon';
 })
 export class AppComponent {
   title = 'kita-flohmarkt-lurup';
+
+  ngOnInit() {
+    const starsContainer = document.querySelector('body')!;
+    const colors = ['#FFFA4D', '#E04DB0', '#5902EC', '#F2C9E1'];
+
+    for (let i = 0; i < 100; i++) {
+      const star = document.createElement('div');
+      star.classList.add('star');
+      star.style.top = `${Math.random() * 100}vh`;
+      star.style.left = `${Math.random() * 100}vw`;
+      star.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+      star.style.width = `${Math.random() * 10 + 5}px`;
+      star.style.height = star.style.width; // Quadratisch
+      star.style.transform = `rotate(${Math.random() * 360}deg)`; // Zufällige Rotation
+      starsContainer.appendChild(star);
+    }
+  }
 }
