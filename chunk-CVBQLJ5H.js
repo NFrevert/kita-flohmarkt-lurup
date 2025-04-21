2f646942caf9ecb0e2a79a6ee9cf9ebce4aca31a
NFrevert <95748982+NFrevert@users.noreply.github.com>
2025-04-21 20:25:58 +0000

    Deploying to gh-pages from @ NFrevert/kita-flohmarkt-lurup@68e7d6f82ec9b469d9f31c42172221e7ae826c8b 🚀

---
+++ chunk-CVBQLJ5H.js
@@ -0,0 +1,6 @@
+import{Ab as a,Bb as l,Cb as r,Ea as n,Fa as e,Na as t,ta as o}from"./chunk-VLHR7KCT.js";var d=class m{static \u0275fac=function(i){return new(i||m)};static \u0275cmp=o({type:m,selectors:[["app-home"]],decls:28,vars:0,template:function(i,s){i&1&&(n(0,"h5")(1,"mat-icon"),t(2,"calendar_month"),e(),t(3,` Wann?
+`),e(),n(4,"p"),t(5,` Am 22.06.2025, von 10.00 bis 14.00 Uhr
+`),e(),n(6,"h5")(7,"mat-icon"),t(8,"apartment"),e(),t(9,` Wo?
+`),e(),n(10,"p"),t(11,"Kita Swatten Weg"),e(),n(12,"p"),t(13,"Swatten Weg 10a"),e(),n(14,"p"),t(15,"22547 Hamburg"),e(),n(16,"h5")(17,"mat-icon"),t(18,"storefront"),e(),t(19,` Was?
+`),e(),n(20,"p"),t(21,"Flohmarkt f\xFCr Kinderbekleidung, Spielzeug und alles, was dazugeh\xF6rt!"),e(),n(22,"h5")(23,"mat-icon"),t(24,"dining"),e(),t(25,` Verpflegung
+`),e(),n(26,"p"),t(27,"Au\xDFerdem ist vor Ort mit Kaffee und Kuchen - gegen einen kleinen Obolus - f\xFCr das leibliche Wohl gesorgt."),e())},dependencies:[a,r,l],encapsulation:2})};export{d as HomeComponent};
