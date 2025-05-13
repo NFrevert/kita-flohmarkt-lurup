78ba39070c1a7b3d1e55dcc65219ec971f6fb96f
NFrevert <nils.frevert@gmail.com>
2025-05-13 20:38:14 +0200

    texte aktualisiert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTooltipModule} from '@angular/material/tooltip'; 
 
 @Component({
   selector: 'app-root',
